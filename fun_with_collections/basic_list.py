d07cde996c1920adf234d463ca2b679b9f6b924b
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-10-05 07:42:06 -0500

    Commit for working project - need to add manual test in main

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -4,23 +4,35 @@ Author: Tyler Hochstetler
 
 
 def make_list():
-  """Returns a list of user input"""
+  """Returns a list of user input
+  :param user_list: List variable to hold list of user input
+  :param num: temporary variable used to hold the return from get_input(), which is stored as an integer and appended to user_list
+  :return: Returns user_list 
+  """
   user_list = []
 
   try:
     for i in range(0, 3):
       num = int(get_input())
       user_list.append(num)
+    return user_list
   except ValueError:
     print("You done messed up.. Need an int")
+    raise ValueError
     
-  return user_list
+  # return user_list
 
 
 def get_input():
-  """Asks user for 3 inputs and returns an array of those inputs"""
-  pass
+  """Asks user for input and return as string.
+  :param user_input: Capture integer from user input
+  :return: user_input as string
+  """
+  user_input = input("Please input a number: ")
+
+  return user_input
 
 
 if __name__ == "__main__":
-    print(make_list())
+    make_a_list = make_list()
+    print(make_a_list)
